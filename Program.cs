61f0109fa74f06bdd481a6c77b12e723b6edfcb8
Sreeragvr1 <sreeragvr02@gmail.com>
2024-11-26 15:53:56 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using FootballclubApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+builder.Services.AddDbContext<MemberDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MemberDBContext")));
+
+var app = builder.Build();
+
+// Middleware
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+//gittest1
